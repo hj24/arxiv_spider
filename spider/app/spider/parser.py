bd542f2f10a6e25111b23a9a284d5b4ba64f7dd1
hj24 <huangjian24@foxmail.com>
2020-01-17 23:32:46 +0800

    tmp commit

--- spider/app/spider/parser.py
+++ spider/app/spider/parser.py
@@ -9,9 +9,11 @@ class PageParser:
     """
     解析arxiv的每一页
     """
-    def __init__(self, html):
+    def __init__(self, html, subject):
         self._soup = BeautifulSoup(html, "html.parser")
         self._tags = None
+        self._content = html
+        self._subj = subject
 
     def _parse_tag_by_class(self, tag_name, tag_class):
         try:
@@ -20,11 +22,36 @@ class PageParser:
             self._tags = []
 
     def parser_page(self):
-        try:
-            self._parse_tag_by_class('li', 'arxiv-result')
-        except Exception:
-            pass
-        else:
-            pass
+        self._parse_tag_by_class('li', 'arxiv-result')
+        for tag in self._tags:
+            str_tag = str(tag)
+            parsed = {
+                'title': self._parse_title(str_tag),
+                'arxurl': self._parse_arxurl(str_tag),
+                'pdfurl': self._parse_pdfurl(str_tag),
+                'author': self._parse_authors(str_tag),
+                'expr': self._parse_expr(str_tag),
+                'content': self._parse_content(str_tag),
+                'subject': self._subj
+            }
+            yield parsed
+
+    def _parse_title(self, content):
+        pass
+
+    def _parse_arxurl(self, content):
+        pass
+
+    def _parse_pdfurl(self, content):
+        pass
+
+    def _parse_authors(self, content):
+        pass
+
+    def _parse_expr(self, content):
+        pass
+
+    def _parse_content(self, content):
+        pass
 
 
--- spider/app/spider/utils.py
+++ spider/app/spider/utils.py
@@ -1,14 +1,14 @@
 import re
 
 
-# 爬取arxiv需要的正则匹配模式
+# 爬取 arxiv 需要的正则匹配模式
 PAGNUMPAT = re.compile(r'Showing.*?&ndash;(\d+).*?of.*?(\d+).*?results.*?for.*?all:')
 
-# 解析title
+# 解析 title
 TITLE = re.compile(r'<p.*?class="title is-5 mathjax">\s+(.*?)\s+</p>')
 
-# 解析arx_url
+# 解析 arx_url
 ARXURL = re.compile(r'<a.*?href="(.*?)">arXiv:.*?</a>')
 
-# 解析pdf_url
+# 解析 pdf_url
 PDFURL = re.compile(r'<a.*?href="(.*?)">pdf</a>')
