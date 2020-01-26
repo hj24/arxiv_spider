0d000c64e91a65e9e9af7f20e031e482225dcd06
hj24 <huangjian24@foxmail.com>
2020-01-26 21:04:14 +0800

    tmp commit

--- spider/app/extensions.py
+++ spider/app/extensions.py
@@ -1,8 +1,5 @@
-from gevent import monkey
-monkey.patch_all()
-
 import redis
-from apscheduler.schedulers.gevent import GeventScheduler
+#from apscheduler.schedulers.gevent import GeventScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -11,7 +8,7 @@ from peeweext.sea import Peeweext
 
 
 config2scheduler = {
-    'gevent': GeventScheduler,
+    #'gevent': GeventScheduler,
     'blocking': BlockingScheduler,
     'background': BackgroundScheduler,
 }
@@ -32,70 +29,70 @@ class Redis:
         return getattr(self._client, name)
 
 ## 定义apscheduler扩展
-class TasksManager:
-
-    def __init__(self):
-        self._scheduler = None
-        self._job_list = []
-
-    def init_app(self, app):
-        opts = app.config.get_namespace('APS_')
-        sch = opts.get('scheduler', 'gevent')
-        self._scheduler = config2scheduler[sch]()
-
-    def __getattr__(self, item):
-        return getattr(self._scheduler, item)
-
-    def add_cron_job_per_week(self, job_id, desc, func, args, d_of_w, hour, min):
-        """
-        添加corn类型的定时任务
-        """
-        try:
-            self._scheduler.add_job(func=func, trigger='cron', id=job_id,
-                                    name=desc, args=args, day_of_week=d_of_w,
-                                    hour=hour, minute=min)
-        except Exception as e:
-            raise e
-        else:
-            self._job_list.append({'job_id': job_id, 'desc': desc})
-
-    def add_job(self, job_id, desc, func, date, args):
-        """
-        添加一次性的定时任务，在run_date指定的时间运行
-        """
-        try:
-            self._scheduler.add_job(func=func, trigger='date', id=job_id,
-                                    name=desc, args=args, run_date=date,
-                                    replace_existing=True)
-        except Exception as e:
-            raise e
-        else:
-            self._job_list.append({'job_id': job_id, 'desc': desc})
-
-    def stop_job(self, job_id):
-        """
-        通过job id来关闭某个定时任务
-        """
-        try:
-            for job in self._job_list:
-                if job['job_id'] == job_id:
-                    self._scheduler.remove_job(job_id)
-                    self._job_list.remove(job)
-                    break
-        except Exception as e:
-            raise e
-
-    def existed_job(self, job_id):
-        try:
-            for job in self._job_list:
-                if job['job_id'] == job_id:
-                    return True
-            return False
-        except Exception as e:
-            raise e
-
-    def get_jobs(self):
-        return self._job_list
+# class TasksManager:
+#
+#     def __init__(self):
+#         self._scheduler = None
+#         self._job_list = []
+#
+#     def init_app(self, app):
+#         opts = app.config.get_namespace('APS_')
+#         sch = opts.get('scheduler', 'gevent')
+#         self._scheduler = config2scheduler[sch]()
+#
+#     def __getattr__(self, item):
+#         return getattr(self._scheduler, item)
+#
+#     def add_cron_job_per_week(self, job_id, desc, func, args, d_of_w, hour, min):
+#         """
+#         添加corn类型的定时任务
+#         """
+#         try:
+#             self._scheduler.add_job(func=func, trigger='cron', id=job_id,
+#                                     name=desc, args=args, day_of_week=d_of_w,
+#                                     hour=hour, minute=min)
+#         except Exception as e:
+#             raise e
+#         else:
+#             self._job_list.append({'job_id': job_id, 'desc': desc})
+#
+#     def add_job(self, job_id, desc, func, date, args):
+#         """
+#         添加一次性的定时任务，在run_date指定的时间运行
+#         """
+#         try:
+#             self._scheduler.add_job(func=func, trigger='date', id=job_id,
+#                                     name=desc, args=args, run_date=date,
+#                                     replace_existing=True)
+#         except Exception as e:
+#             raise e
+#         else:
+#             self._job_list.append({'job_id': job_id, 'desc': desc})
+#
+#     def stop_job(self, job_id):
+#         """
+#         通过job id来关闭某个定时任务
+#         """
+#         try:
+#             for job in self._job_list:
+#                 if job['job_id'] == job_id:
+#                     self._scheduler.remove_job(job_id)
+#                     self._job_list.remove(job)
+#                     break
+#         except Exception as e:
+#             raise e
+#
+#     def existed_job(self, job_id):
+#         try:
+#             for job in self._job_list:
+#                 if job['job_id'] == job_id:
+#                     return True
+#             return False
+#         except Exception as e:
+#             raise e
+#
+#     def get_jobs(self):
+#         return self._job_list
 
 
 # 使用扩展
@@ -110,4 +107,4 @@ pwdb = Peeweext(ns='PW_')
 spredis = Redis()
 
 ## 定时任务扩展
-tasker = TasksManager()
+#tasker = TasksManager()
--- spider/app/servicers.py
+++ spider/app/servicers.py
@@ -1,9 +1,73 @@
-# import spider_pb2
-# import spider_pb2_grpc
+import spidermanager_pb2
+import spidermanager_pb2_grpc
 
-# from sea.servicer import ServicerMeta
+from sea.servicer import ServicerMeta
+
+# from datetime import datetime
+#
+# from app.extensions import tasker
+# from app.utils import random_date
+# from app.spider.main import Engine
 
 
-# class SpiderServicer(spider_pb2_grpc.SpiderServicer, metaclass=ServicerMeta):
+class SpiderManagerServicer(spidermanager_pb2_grpc.SpiderServicer,
+                            metaclass=ServicerMeta):
+    """
+    对外提供的爬虫管理服务，定义在spidermanager.proto文件中
+    定义规范参考Protobuf
+    """
+    def SpiderConn(self, request, context):
 
-#     pass
+        # def setup_spider():
+        #     _date = random_date()
+        #     _year = _date.year
+        #     _month = _date.month
+        #     _day = _date.day
+        #     _hour = _date.hour
+        #     _minutes = _date.minute
+        #     _engine = Engine()
+        #     print('setip spider &&&&&&&&&&&&&&&&&&&&&&&&')
+        #     tasker.add_job(job_id='spider', desc='spider task', func=_engine.loop,
+        #                    date=datetime(year=2020, month=1,
+        #                                  day=26, hour=17, minute=23),
+        #                    args=[])
+
+        try:
+            print('service ******' + request.keyswitch)
+            if request.keyswitch == 'on':
+                # tasker.add_cron_job_per_week(job_id='setup', desc='setup a spider job',
+                #                              func=setup_spider, args=[], d_of_w='sun',
+                #                              hour=0, min=0)
+                #Engine().loop()
+                print('service ******')
+            elif request.keyswitch == 'off':
+                # if tasker.existed_job('spider'):
+                #     tasker.stop_job('spider')
+                pass
+            else:
+                return spidermanager_pb2.ConnReply(status='unknow', message=request.keyswitch)
+        except Exception:
+            return spidermanager_pb2.ConnReply(status='failed', message=request.keyswitch)
+        else:
+            return spidermanager_pb2.ConnReply(status='success', message=request.keyswitch)
+
+    def TasksList(self, request, context):
+        # try:
+        #     replay_body = spidermanager_pb2.ListReply()
+        #     if request.query == 'lists':
+        #         job_list = tasker.get_jobs()
+        #         for job in job_list:
+        #             task = replay_body.JobList.add()
+        #             task.jobid = job['job_id']
+        #             task.detail = job['details']
+        #         replay_body.status = 'success'
+        #     else:
+        #         raise Exception('unkonw args')
+        # except Exception:
+        #     _replay_body = spidermanager_pb2.ListReply()
+        #     _replay_body.status = 'failed'
+        #     _replay_body.JobList.add()
+        #     return _replay_body
+        # else:
+        #     return replay_body
+        pass
