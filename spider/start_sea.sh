1f4a071125e17d20e3a95aeb828a25abde986805
hj24 <huangjian24@foxmail.com>
2020-02-01 18:40:15 +0800

    update deploy files.

--- spider/start_sea.sh
+++ spider/start_sea.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-nohup sea server &
-nohup sea async_task beat &
-nohup sea async_task worker &
+nohup sea server > sea.log 2>1& &
+nohup sea async_task beat > beat.log 2>&1 &
+nohup sea async_task worker > worker.log 2>&1 &
