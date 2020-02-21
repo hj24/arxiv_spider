f4238094ef78175bb4e53f4f0a50b408945bf43a
hj24 <huangjian24@foxmail.com>
2020-02-21 16:45:34 +0800

    update for build

--- spider/app/__init__.py
+++ spider/app/__init__.py
@@ -21,7 +21,7 @@ def register(sender):
     # 健康检查的ip，端口，检查时间
     check = consul.Check.tcp(ip, port, "10s")
     # 注册服务部分
-    c.agent.service.register(server_name, f"{server_name}-{ip}-{port}",
+    c.agent.service.register(server_name, f"{server_name}",
                             address=ip, port=port, check=check) 
     print(f"注册服务{server_name}成功")
 
@@ -31,10 +31,8 @@ def unregister(sender):
     cfg = current_app.config.get_namespace("CONSUL_")
     c = consul.Consul(host=cfg["server_host"])
     server_name = cfg["server_name"]
-    ip = cfg["register_ip"]
-    port = cfg["register_port"]
     print(f"开始退出服务{server_name}")
-    c.agent.service.deregister(f'{server_name}-{ip}-{port}')
+    c.agent.service.deregister(f'{server_name}')
 
 # 接收到server创建的信号时注册服务
 from sea.signals import server_stopped, server_started
--- spider/configs/production.py
+++ spider/configs/production.py
@@ -31,7 +31,7 @@ ASYNC_TASK_TIMEZONE = TIMEZONE
 ASYNC_TASK_BEAT_SCHEDULE = {
     'execute_per_week': {
         'task': 'app.async_tasks.run_spider',
-        'schedule': crontab(day_of_week=5, hour=15, minute=45),
+        'schedule': crontab(day_of_week=0, hour=7, minute=30),
         #'args': (1, 2, 3)
     }
 }
